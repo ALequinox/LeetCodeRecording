47b42ee6fe6ce448a9df0421b1a7692b3747b1cf
ALequinox <1397344525@qq.com>
2021-04-24 23:28:06 +0800

    update

---
+++ record/src/main/java/Test4.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test4 {
+    /*
+    剑指 Offer 06. 从尾到头打印链表
+    输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+    输入：head = [1,3,2]
+    输出：[2,3,1]
+     */
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        head.next = node2;
+        node2.next = node3;
+        node3.next = null;
+
+        System.out.println(Arrays.toString(reversePrint(head)));
+    }
+
+    private   static  int[] reversePrint(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode p = head;
+        while(p!=null){
+            list.add(p.val);
+            p = p.next;
+        }
+        int[] r = new int[list.size()];
+        for (int i = list.size()-1; i >= 0; i--) {
+            r[list.size()-1-i] = list.get(i);
+        }
+        return r;
+    }
+
+    static class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+    }
+}
