ef52b0a3a4df973ec47e022dee9a1826436ec695
ALequinox <1397344525@qq.com>
2021-05-29 12:37:35 +0800

    update

---
+++ record/src/main/java/Test19.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * 题解：
+ * https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/solution/ti-jie-shou-wei-shuang-zhi-zhen-kuai-man-shuang-zh/
+ */
+public class Test19 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        System.out.println(Arrays.toString(exchange(a)));
+    }
+
+
+    private static int[] exchange(int[] nums) {
+        int hi = nums.length-1,low = 0;
+        int len = nums.length;
+        while (hi>low){
+            if(nums[low] % 2 != 0 ){
+               low++;
+               continue;
+            }
+            if(nums[hi] % 2 == 0 ) {
+                hi--;
+                continue;
+            }
+            int temp = nums[hi];
+            nums[hi--] = nums[low];
+            nums[low++] = temp;
+        }
+
+        return nums;
+    }
+}
