d6ddab62ede1da6cbc7cc0f04cf61da9ce39622f
ALequinox <1397344525@qq.com>
2021-06-08 14:02:41 +0800

    update

--- record/src/main/java/TreeNode.java
+++ record/src/main/java/TreeNode.java
@@ -3,4 +3,13 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
